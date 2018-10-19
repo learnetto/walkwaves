5e0fca9a5494a995daa0d9516d7b9d7286b87edb
Hrishi Mittal <hrishimittal@gmail.com>
2018-10-19 13:52:45 +0100

    Add React Native Elements. Add layout, styles and form for geocoding.

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.title}>Walkwaves</Text>
+        <FormLabel style={{padding: 20}}>Enter Co-ordinates (latitude, longitude)</FormLabel>
+        <FormInput containerStyle={styles.input} />
+        <Button title='Geocode'
+          buttonStyle={styles.button}
+          color='#FFFFFF'
+        />
+        <Text style={styles.result}>Human understandable address</Text>
       </View>
     );
   }
@@ -14,8 +22,24 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F3F3F3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 30,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    width: '80%',
+  },
+  button: {
+    backgroundColor: '#1E88E5',
+    width: 200,
+  },
+  result: {
+    padding: 10,
+  }
 });
